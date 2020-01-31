cfb0f346be2aaf95ce12186aa3359dad663e0935
Kevin <galani.kevin@gmail.com>
2020-01-30 19:15:43 -0500

    The Warm Up

---
+++ PracticeApps/HelloWorld2/HelloWorld2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld2
+//
+//  Created by Kevin G on 2020-01-26.
+//  Copyright © 2020 Kevin G. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PracticeApps/WarCardGame/WarCardGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WarCardGame
+//
+//  Created by Kevin G on 2020-01-27.
+//  Copyright © 2020 Kevin G. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
